97d51fce070f0220750478913f2a694bd046b485
Ringo Suen <Ringo@Ringos-MacBook-Pro.local>
2018-12-04 04:55:20 -0800

    media q

--- js/data.js
+++ js/data.js
@@ -2,15 +2,15 @@
 let listOfBaes = 
 "<ul id='container'>" +
 "<li><div class='show'><img src='../images/bae1.jpg' alt='test_image' class='img_char'><p class='name_char'>Shikori Miyokava<br><span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span> </p></div></li>" +
-"<li><div class='bae_list'><img src='../images/bae2.jpg' alt='test_image' class='img_char'><p class='name_char'>Alice <br><span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" +
+"<li><div class='bae_list'><img src='../images/bae2.jpg' alt='test_image' class='img_char'><p class='name_char'>Alice Lee<br><span class='about_me'><br/> Help me get better at languages! <br> I run 10km a day! Come chat with about it! </span></p></div></li>" +
 "<li><div class='bae_list'><img src='../images/bae3.jpg' alt='test_image' class='img_char'><p class='name_char'>Karen <br><span class='about_me'><br/> I am into cheesy pick-up lines.  <br> People often describes me as athletic, funny, bold, and sociable. </span></p></div></li>" +
-"<li><div class='bae_list'><img src='../images/bae4.png' alt='test_image' class='img_char'><p class='name_char'>Lizzy <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" +
-"<li><div class='bae_list'><img src='../images/bae5.jpeg' alt='test_image' class='img_char'><p class='name_char'>Emma <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" +
-"<li><div class='bae_list'><img src='../images/bae6.jpeg' alt='test_image' class='img_char'><p class='name_char'>Chloe <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" +
-"<li><div class='bae_list'><img src='../images/bae7.jpeg' alt='test_image' class='img_char'><p class='name_char'>Ellie  <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" + 
-"<li><div class='bae_list'><img src='../images/bae8.jpeg' alt='test_image' class='img_char'><p class='name_char'>Rochelle  <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" + 
-"<li><div class='bae_list'><img src='../images/bae9.jpg' alt='test_image' class='img_char'><p class='name_char'>Angela  <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></li>" + 
-"<li><div class='bae_list'><img src='../images/bae10.jpg' alt='test_image' class='img_char'><p class='name_char'>Sara  <span class='about_me'><br/> Help me get better at languages! <br> I speak some Vietnamese, Japanese, Korean, and I really want to learn Mandarin! </span></p></div></p></div></li>" 
+"<li><div class='bae_list'><img src='../images/bae4.png' alt='test_image' class='img_char'><p class='name_char'>Lizzy Cheung <br> <span class='about_me'><br/> Help me get better at languages! <br> I really want to learn Spanish! </span></p></div></li>" +
+"<li><div class='bae_list'><img src='../images/bae5.jpeg' alt='test_image' class='img_char'><p class='name_char'>Emma Wilburn <br><span class='about_me'><br/> Help me get better at languages! <br> I enjoy long walks on beaches. </span></p></div></li>" +
+"<li><div class='bae_list'><img src='../images/bae6.jpeg' alt='test_image' class='img_char'><p class='name_char'>Chloe Ngo <br> <span class='about_me'><br/> Pizza or nothing! <br> I drink bubble tea everyday! </span></p></div></li>" +
+"<li><div class='bae_list'><img src='../images/bae7.jpeg' alt='test_image' class='img_char'><p class='name_char'>Ellie Chan <br><span class='about_me'><br/> Help me get better at languages! <br> I speak some French, Japanese, Chinese,  </span></p></div></li>" + 
+"<li><div class='bae_list'><img src='../images/bae8.jpeg' alt='test_image' class='img_char'><p class='name_char'>Rochelle Ma <br> <span class='about_me'><br/> Bubble tea Bubble tea! Bubble tea <br>I really <br> want to learn how to make it! </span></p></div></li>" + 
+"<li><div class='bae_list'><img src='../images/bae9.jpeg' alt='test_image' class='img_char'><p class='name_char'>Angela Lo <br> <span class='about_me'><br/> Help me get better at english! <br> I speak some Vietnamese, Chinese, Korean, and I really want to learn Russian! </span></p></div></li>" + 
+"<li><div class='bae_list'><img src='../images/bae10.png' alt='test_image' class='img_char'><p class='name_char'>Sara Chen  <br> <span class='about_me'><br/> I enjoy baking and cooking! <br> I love traveling as well. <br> Always down for bubble tea.</span></p></div></p></div></li>" 
 "</ul>";
 
 //let someOtherData = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
@@ -56,12 +56,12 @@ let galleryOfBaes =
   class: 'gallery_bae'
 },
 {
-  src: '../images/bae9.jpg',
+  src: '../images/bae9.jpeg',
   alt: 'test_image',
   class: 'gallery_bae'
 },
 {
-  src: '../images/bae10.jpg',
+  src: '../images/bae10.png',
   alt: 'test_image',
   class: 'gallery_bae'
 }];
